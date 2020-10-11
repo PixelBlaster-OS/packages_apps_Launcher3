906d4c963c383bc97a86e904e6ffccb5f981ab92
Anay Wadhera <awadhera@berkeley.edu>
2020-10-10 22:46:40 -0700

    Launcher3: Google feed integration
    Change-Id: I4dd635369513e7e8db8f8e4363acb017e0ce8eb1
    [kdrag0n: Debranded to com.android.launcher3]
    [kdrag0n: Ported to Android 12 and converted libGoogleFeed to Soong]
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- quickstep/src/com/blaster/launcher/BlasterLauncher.java
+++ quickstep/src/com/blaster/launcher/BlasterLauncher.java
@@ -18,7 +18,13 @@
 package com.blaster.launcher;
 
 import com.android.launcher3.uioverrides.QuickstepLauncher;
+import com.android.systemui.plugins.shared.LauncherOverlayManager;
 
 public class BlasterLauncher extends QuickstepLauncher {
+
+    @Override
+    protected LauncherOverlayManager getDefaultOverlay() {
+        return new OverlayCallbackImpl(this);
+    }
 
 }
